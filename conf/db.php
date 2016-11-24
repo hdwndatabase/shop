9d88e85870dbc0016647d98a86b59741100392dd
hdwndatabase <1205728818@qq.com>
2016-11-24 21:33:33 +0800

    123

--- conf/db.php
+++ conf/db.php
@@ -1,7 +1,7 @@
 <?php
     define('DB_HOST','localhost');
     define('DB_USERNAME','root');
-    define('DB_PWD','carotpa');
+    define('DB_PWD','123');
     define('DB_NAME','shop');
 
     $db = new mysqli(DB_HOST, DB_USERNAME, DB_PWD, DB_NAME) or die('连接数据库失败');
