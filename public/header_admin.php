49c6d7d4c5717eece7c2b25d7a0d5a54fbeb4cb5
hdwndatabase <1205728818@qq.com>
2016-11-24 22:27:01 +0800

    page

--- public/header_admin.php
+++ public/header_admin.php
@@ -5,6 +5,23 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="css/style.css">
+    <style type="text/css">
+        *{
+            padding: 0px;
+            margin:0px;
+        }
+        .header{
+            width:100%;
+            height:33px;
+            background: url(/shop/public/images/background.gif) repeat-x;
+            border-style: solid;
+            border-color: red;
+            font-weight: 700;
+            font-family: 微软雅黑;
+            font-size: 18px;
+            line-height: 31px;
+        }
+    </style>
 </head>
 <body>
 <div class="header">
@@ -13,13 +30,6 @@
         if (isset($_SESSION['admin'])) {
             echo $_SESSION['admin'];
             ?>
-            <div class="sth">
-                <a href="/shop/admin.php">管理员主页</a> |
-                <a href="/shop/view/admin/Manage/good/good.php">商品管理</a> |
-                <a href="/shop/view/admin/Manage/category/category.php">分类管理</a> |
-                <a href="/shop/view/admin/Manage/brand/brand.php">品牌管理</a> |
-                <a href="/shop/logout.php">注销</a>
-            </div>
             <?php
         } else {
             ?>
@@ -27,8 +37,13 @@
             <?php
         }
     ?>
-
-    <hr>
 </div>
+            <div class="sth">
+                <a href="/shop/admin.php">管理员主页</a> |
+                <a href="/shop/view/admin/Manage/good/good.php">商品管理</a> |
+                <a href="/shop/view/admin/Manage/category/category.php">分类管理</a> |
+                <a href="/shop/view/admin/Manage/brand/brand.php">品牌管理</a> |
+                <a href="/shop/logout.php">注销</a>
+            </div>
 </body>
 </html>
--- view/admin/adminPage.php
+++ view/admin/adminPage.php
@@ -1,5 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		ul{
+			list-style: none;
+		}
+	</style>
+</head>
+<body>
 <ul>
     <li><a href="./view/admin/Manage/good/good.php">商品管理</a></li>
     <li><a href="./view/admin/Manage/category/category.php">分类管理</a></li>
     <li><a href="./view/admin/Manage/brand/brand.php">品牌管理</a></li>
 </ul>
+</body>
+</html>
