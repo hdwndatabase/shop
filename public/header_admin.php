c2fd1e9ab2cce6df091525724c2ace6e36d39560
hdwndatabase <1205728818@qq.com>
2016-11-25 20:02:48 +0800

    header_admin-CSS

--- public/header_admin.php
+++ public/header_admin.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,16 +13,57 @@
             width:100%;
             height:33px;
             background: url(/shop/public/images/background.gif) repeat-x;
-            border-style: solid;
-            border-color: red;
             font-weight: 700;
             font-family: 微软雅黑;
             font-size: 18px;
             line-height: 31px;
+            padding-left: 30px;
         }
+        .blank{
+            width:100%;
+            height:33px;
+        }
+        .sth{
+            width:1200px;
+            height:49px;
+            margin:0 auto;
+        }
+        .sth .guanliyuanxuanxiang{
+        width:260px;
+        height:49px;
+        background: url(/shop/public/images/quanbushangpinfenlei.bmp) repeat-x;
+        float:left;
+        }
+        .sth .guanliyuanxuanxiang p{
+        text-align: center;
+        line-height: 49px;
+        font-family: 微软雅黑;
+        color:white;
+        font-size:17px;
+        }
+    .sth .list{
+        width:940px;
+        height:49px;
+        float:left;
+    }
+    .sth .list li a{
+        width:188px;
+        height:49px;
+        float:left;
+        line-height: 49px;
+        background: rgb(239,239,239);
+        font-size: 16px;
+        text-align: center;
+        display: block;
+        font-family: 微软雅黑;
+        text-decoration: none;
+    }
+    .sth .list li a:hover{
+        background: rgb(229,230,230);
+    }
     </style>
 </head>
-<body>
+<body style="background: url(/shop/public/images/bg1.jpg);">
 <div class="header">
     欢迎你，管理员:
     <?php
@@ -38,12 +78,22 @@
         }
     ?>
 </div>
+
+<div class="blank">
+    
+</div>
             <div class="sth">
-                <a href="/shop/admin.php">管理员主页</a> |
-                <a href="/shop/view/admin/Manage/good/good.php">商品管理</a> |
-                <a href="/shop/view/admin/Manage/category/category.php">分类管理</a> |
-                <a href="/shop/view/admin/Manage/brand/brand.php">品牌管理</a> |
-                <a href="/shop/logout.php">注销</a>
+                <div class="guanliyuanxuanxiang"><p>管理员选项</p></div>
+                <div class="list">
+                   <ul>
+                    <li><a href="/shop/admin.php">管理员主页</a> </li>
+                    <li><a href="/shop/view/admin/Manage/good/good.php">商品管理</a></li>
+                    <li><a href="/shop/view/admin/Manage/category/category.php">分类管理</a></li>
+                    <li><a href="/shop/view/admin/Manage/brand/brand.php">品牌管理</a></li>
+                    <li><a href="/shop/logout.php">注销</a></li>
+                    </ul> 
+                </div>
+                               
             </div>
 </body>
 </html>
