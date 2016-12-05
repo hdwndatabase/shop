ce7a3d95a078c78000913affbacb246deda26f67
hdwndatabase <1205728818@qq.com>
2016-12-05 21:17:20 +0800

    change success.php

--- view/user/info/modify/pwdErr.php
+++ view/user/info/modify/pwdErr.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+    <style type="text/css">
+    h2{
+    	margin-top: 50px;
+    	margin-left: 200px;
+    }
+    p{
+    	font-family: 楷体;
+    	font-size: 22px;
+    	font-weight: 600;
+    	margin-left: 450px;
+    	margin-top: 15px;
+    }
+    </style>
+</head>
+<body  style="background: url(/shop/public/images/bg1.jpg);">
+
+</body>
+</html>
 <?php
     session_start();
     require_once '../../../../conf/db.php';
